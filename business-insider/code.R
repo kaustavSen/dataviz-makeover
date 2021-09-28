991dec9ba64deae58378dc08a0a1f30e6a924152
Kaustav Sen <sen_kaustav@hotmail.com>
2021-09-28 21:28:28 +0530

    bar plot added to table

--- business-insider/code.R
+++ business-insider/code.R
@@ -1,16 +1,45 @@
 library(tidyverse)
 library(gt)
+library(ggforce)
 
 data <- tribble(
-  ~industry, ~`2020`, ~`2025`,
-  "Wealth management", 0, 2.6,
-  "Consumer lending", 1.4, 15.7,
-  "Insurance", 5, 70.7,
-  "Payments", 16.1, 140.8
+  ~industry, ~`2020`, ~`2025`, ~ggplot,
+  "Wealth management", 0, 2.6, NA,
+  "Consumer lending", 1.4, 15.7, NA,
+  "Insurance", 5, 70.7, NA,
+  "Payments", 16.1, 140.8, NA,
 )
 
-table <- 
-  gt(data) %>% 
+plot_bars <- function(data) {
+  bar_length <- data[["2025"]]
+  df <- tibble(x = c(0, bar_length, bar_length, 0),
+               y = c(0, 0, 1, 1))
+  
+  ggplot(df, aes(x, y)) +
+    geom_shape(radius = unit(0.8, "cm"), fill = "grey40") +
+    scale_x_continuous(limits = c(0, 141)) +
+    theme_void() +
+    coord_cartesian(clip = "off")
+  
+}
+
+plots <- data %>% 
+  select(-ggplot) %>% 
+  arrange(desc(`2025`)) %>% 
+  group_by(industry) %>% 
+  nest() %>% 
+  mutate(plot = map(data, plot_bars))
+
+table <-
+data %>% 
+  arrange(desc(`2025`)) %>% 
+  gt() %>% 
+  text_transform(
+    locations = cells_body(columns = ggplot),
+    fn = function(x) {
+      map(plots$plot, ggplot_image, height = 20, aspect_ratio = 6)
+    }
+  ) %>% 
   tab_header(
     title = "FORECAST: Embedded Finance Market Value",
     subtitle = "Generated revenues, USD billions"
@@ -20,7 +49,8 @@ table <-
   ) %>% 
   opt_align_table_header(align = "left") %>% 
   cols_label(
-    industry = ""
+    industry = "",
+    ggplot = ""
   ) %>% 
   tab_style(
     style = cell_text(
@@ -49,6 +79,7 @@ table <-
       font = google_font("Inconsolata")
     ),
     locations = cells_body(columns = -industry)
-  )
+  ) %>% 
+  cols_width(ggplot ~ px(100))
 
 gtsave(table, "BI_Table.png", expand = 5, path = "business-insider")
